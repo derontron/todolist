21c27472b6f94af5a0169d101f488dfd5adf8c46
derontron <derontron@gmail.com>
2016-07-05 18:18:26 -0400

    Complete functionality with write, delete and crossout

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -19,13 +19,22 @@
 
 // });
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input").keypress(function(event){
+	// grab text from input once enter is pressed
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		// create a new li and add to ul
+		$('ul').append('<li><span>X </span>' + todoText + '</li>');
+	}
 });
