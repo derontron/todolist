a09ef066edb5b4444512d2a6f4272dbab3fed1c4
derontron <derontron@gmail.com>
2016-07-05 00:53:08 -0400

    Hardcoded todo list with ability to crossout items stylistically

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,21 @@
-alert('connected');
+// check off specific todos by clicking
+
+$('li').click(function(){
+	// if li is gray
+	if($(this).css("color") === "rgb(128, 128, 128)") {
+		// turn it black;
+		$(this).css({
+			color: "black",
+			textDecoration: "none"
+	});
+	
+	} else {
+		//turn it gray;
+		$(this).css({
+			color: "grey",
+			textDecoration: "line-through"
+		});
+	}
+
+	
+});
