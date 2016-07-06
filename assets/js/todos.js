e29a2eea9514f8d6cf679da4851f3f3b194397a8
derontron <derontron@gmail.com>
2016-07-06 15:49:56 -0400

    Complete app with css animations and font awesome additions

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -35,6 +35,10 @@ $("input").keypress(function(event){
 	if (event.which === 13) {
 		var todoText = $(this).val();
 		// create a new li and add to ul
-		$('ul').append('<li><span>X </span>' + todoText + '</li>');
+		$('ul').append('<li><span><i class="fa fa-trash"></i></span>' + todoText + '</li>');
 	}
+});
+
+$('.fa-plus').click(function(){
+	$("input[type='text']").fadeToggle();
 });
